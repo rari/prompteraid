8854b7f0b9e3d0785e86e874f67764f9676a6ac5
rari <imiko.rari@gmail.com>
2025-07-03 16:41:12 -0400

    new deployment workflow

--- scripts/deploy/deploy_to_master.bat
+++ scripts/deploy/deploy_to_master.bat
@@ -5,12 +5,11 @@ REM This script deploys changes from explore branch to master
 echo 🚀 Deploying PrompterAid to master branch...
 echo.
 
-REM Change to the project root directory
-cd ..
+REM Get the directory of this script
+set SCRIPT_DIR=%~dp0
 
-REM Run the Python deployment script
-python scripts/deploy/deploy_to_master.py
+REM Run the Python deployment script using the full path
+python "%SCRIPT_DIR%deploy_to_master.py"
 
-REM Pause to show results
 echo.
 pause 
